1e9e55a443e5cfa83f40f12c644030000ba07728
Rudy Seidinger <rudyseidinger@gmail.com>
2012-08-27 12:35:48 -0300

    Finalizando modelo inicial de filtros para dashboards - checkbox filter

--- js/checkbox-filter.js
+++ js/checkbox-filter.js
@@ -5,30 +5,54 @@ angular.module('components',[])
       scope : {},
       compile: function(tElement, tAttr, transclude){
         var inputs = tElement.find('input.values');        
-        console.debug(inputs);
+        var elementsContainers = tElement.find('.filter_box');
         angular.forEach(inputs, function(input){
           var inputElemnt = angular.element(input);
-          inputElemnt.attr('ng-click', 'registerValue("'+inputElemnt.val()+'")');
+          inputElemnt.attr('ng-click', 'registerValue("'+inputElemnt.val()+'")');          
+        });
+
+        angular.forEach(elementsContainers, function(element){
+          var postElement = angular.element(element);
+          postElement.prepend('<div class="header_f1"><input type="checkbox" class="all" ng-click="markAll()"><label>Todos</label></div>')
         });
         
-        //link function
         return function(scope, element, attrs){
+          scope.all = true;
           scope.filter_values = [];
+          scope.elementOptions = element.find('.values');
+          scope.elementAllToggle = element.find('.all');
+          scope.totalNumberOfOptions = scope.elementOptions.length;
           scope.filter_field = element.data('field');
           
           scope.registerValue = function(value){
-            //tenho que inlcuir em filter_values o valor do elemento
-            if ( _.indexOf(scope.filter_values, value) == -1){
+            if ( _.indexOf(scope.filter_values, value) == -1){              
               scope.filter_values.push(value);
             }
             else{
               scope.filter_values = _.without(scope.filter_values, value);
-            }
-            console.log(scope.filter_values);
-            console.log(scope.filter_field);
+            }            
+            scope.all = (scope.filter_values.length == scope.totalNumberOfOptions);
           };
+
+          scope.markAll = function(){
+            if (scope.all)
+              scope.elementAllToggle.attr('checked','checked');
+            else  
+              scope.all = true;
+          }
+
+          scope.$watch('all', function(newValue, oldValue){            
+            if (newValue == true)
+              angular.forEach(scope.elementOptions, function(el){
+                angular.element(el).attr('checked', 'checked');
+                var value = angular.element(el).val();
+                if ( _.indexOf(scope.filter_values, value) == -1){
+                  scope.filter_values.push(value);
+                };
+              });
+            scope.elementAllToggle.attr('checked', newValue);
+          });
         };
-        
       }
     };
   });
